57129dbd4ab26514ad7bf5261809161157672f91
linimbus <linimbus@126.com>
2018-06-06 22:13:48 +0800

    add test

--- example/main.go
+++ example/main.go
@@ -15,11 +15,50 @@ func (hello) Test01(s *gounit.Suite) {
 	s.ASSERT(true)
 }
 
+func (hello) Test02(s *gounit.Suite) {
+
+	log.Println("test run ....")
+
+	s.ASSERT(true)
+}
+
+func (hello) Test03(s *gounit.Suite) {
+
+	log.Println("test run ....")
+
+	s.ASSERT(true)
+}
+
+type world int
+
+func (world) Test01(s *gounit.Suite) {
+
+	log.Println("test run ....")
+
+	s.ASSERT(true)
+}
+
+func (world) Test02(s *gounit.Suite) {
+
+	log.Println("test run ....")
+
+	s.ASSERT(true)
+}
+
+func (world) Test03(s *gounit.Suite) {
+
+	log.Println("test run ....")
+
+	s.ASSERT(true)
+}
+
 func main() {
 
 	var a hello
+	var b world
 
 	gounit.AddSuite(&a)
+	gounit.AddSuite(&b)
 
 	gounit.RunAllTests()
 
